ed4cf6eee95346c62543d7d60d5f99ab99eb8c6a
Nicolas Laplante <nicolas.laplante@gmail.com>
2012-06-29 15:23:20 -0400

    Complete the dustr controller

--- WebContent/scripts/compiler.js
+++ WebContent/scripts/compiler.js
@@ -1,11 +1,7 @@
 /*!
- * Online DustJS compiler Angular controller script
- * 
- * $Id: compiler.js 10 2012-03-20 22:06:41Z nlaplante $
- * 
+ * Online DustJS compiler Angular controller script * 
  * Written by Nicolas Laplante (nicolas.laplante@gmail.com)
- */
- 
+ */ 
 function DustrCtrl($scope)
 {
 	// Input & output fields
@@ -16,14 +12,27 @@ function DustrCtrl($scope)
 	});
 	
 	// Events
-	$scope.evtCompile = function () {
-		
+	$scope.compile = function () {
+		$scope.output = dust.compile($scope.source, $scope.name);
 	};
 	
 	// Handler to clear the fields
-	$scope.evtClear = function () {
+	$scope.clear = function () {
 		$scope.source = null;
 		$scope.name = null;
 		$scope.output = null;
 	};
+	
+	// Handler to determine if compile and reset buttons should be enabled/disabled
+	$scope.isUnchanged = function () {
+		return $scope.source == null
+			&& $scope.name == null;
+	};
+	
+	// js_beautify() when setting the output
+	$scope.$watch("output", function (newValue, oldValue) {
+		if (newValue !== null) {
+			$scope.output = js_beautify(newValue);
+		}
+	});
 }
